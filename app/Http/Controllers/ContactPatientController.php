5c21bfb74ff0c878cc5189d93a3629749e4b7c4b
alif0ns0 <alfonso.aguayo.ab@gmail.com>
2023-03-02 17:22:58 +0100

    Estructura de los controladores

---
+++ app/Http/Controllers/ContactPatientController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactPatientController extends Controller
+{
+    public function attach(Request $request)
+    {
+
+    }
+
+    public function detach(Request $request)
+    {
+
+    }
+}
---
+++ app/Http/Controllers/MedicationPatientController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MedicationPatientController extends Controller
+{
+    public function attach(Request $request)
+    {
+    }
+
+    public function detach(Request $request)
+    {
+    }
+}
